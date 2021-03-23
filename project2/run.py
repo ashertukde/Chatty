267753bc4e1661ad936f50d2ecfba2d86bbc3848
Charles Bonoan <cbonoan@prosoft-technology.com>
2021-03-22 22:35:59 -0700

    creating final touches to project

--- project2/run.py
+++ project2/run.py
@@ -3,6 +3,7 @@ from flask_socketio import SocketIO, send, join_room, leave_room, emit
 from flask_session import Session
 from profanity_filter import ProfanityFilter
 import sys
+import random
 
 app = Flask(__name__)
 app.debug = True
@@ -11,9 +12,9 @@ app.config['SESSION_TYPE'] = 'filesystem'
 
 Session(app)
 socketio = SocketIO(app, manage_session=False)
-
-pf = ProfanityFilter()
-
+memberlist = {}
+numOfMessages = {}
+#pf = ProfanityFilter()
 # TODO
 # Add profanity filter for username and room name
 # Create members box to display current members in room
@@ -35,6 +36,13 @@ def chat():
         # when user is in chat room
         session['username'] = username
         session['room'] = room
+        # Generate a random color user will get for chat distinguishing purposes
+        rand = lambda: random.randint(0,255)
+        session['color'] = "#%02X%02X%02X" % (rand(), rand(), rand())
+        if room in memberlist:
+            memberlist[room].append(username)
+        else:
+            memberlist[room] = [username]
         return render_template('chat.html', session=session, title=room)
     else:
         if session.get('username') is not None:
@@ -48,29 +56,40 @@ def chat():
 def join(message):
     room = session.get('room')
     join_room(room)
-    emit('status', {'msg': session.get('username') + ' has entered the room. Say hi!'}, room=room)
+    #members list won't keep track of someone logging in without cookies
+    if session.get('username'):
+        emit('status', {'msg': session.get('username') + ' has entered the room. Say hi!'}, room=room)
+        emit('memberlist', {'msg': memberlist[room]}, room=room)
+    else:
+        emit('status', {'msg': 'Guest has entered the room. Say hi!'}, room=room)
+    
 
 @socketio.on('text', namespace='/chat')
 def text(message):
     room = session.get('room')
     print('Message: ' + message['msg'])
     # Check if message is appropriate
-    if pf.is_clean(message['msg']):
-        emit('message', {'msg': session.get('username') + ': ' + message['msg']}, room=room)
-    else:
-        emit('message', {'msg': 'Detected foul language. Please keep messages clean!'}, broadcast=False, include_self=True)
+    # if pf.is_clean(message['msg']):
+    #     if session.get('username'):
+    #         emit('message', {'msg': session.get('username') + ': ' + message['msg']}, room=room)
+    #     else:
+    #         emit('message',{'msg': 'Guest: ' + message['msg']}, room=room)
+    # else:
+    #     emit('message', {'msg': 'Detected foul language. Please keep messages clean!'}, broadcast=False, include_self=True)
+    emit('message', {'color': session.get('color'), 'username': session.get('username'), 'msg': message['msg']}, room=room)
+
+    
 
 @socketio.on('left', namespace='/chat')
 def left(message):
     room = session.get('room')
     username = session.get('username')
+    memberlist[room].remove(username)
     leave_room(room)
     session.clear()
     emit('status', {'msg': username + ' has left the room.'}, room=room)
-
+    emit('memberlist', {'msg': memberlist[room]}, room=room)
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        socketio.run(app)
-    else:
-        socketio.run(app, host=sys.argv[1])
+    #app.run()
+    socketio.run(app, debug=True)
